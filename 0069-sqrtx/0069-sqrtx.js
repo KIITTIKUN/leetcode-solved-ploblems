afd3bd8904400eebd54583d4b8512722460ee3b6
gparin <99040393+KIITTIKUN@users.noreply.github.com>
2023-07-11 03:33:04 +0700

    Time: 128 ms (8.32%), Space: 43.8 MB (44.76%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.js
+++ 0069-sqrtx/0069-sqrtx.js
@@ -3,6 +3,10 @@
  * @return {number}
  */
 const mySqrt = (x) => {
+    if (x === 0 || x === 1){
+        return x
+    }
+    
     for (let i = 0; i <= x/2+1 ;i++){
         if(i*i > x){
             return i-1
