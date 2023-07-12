35d7d517225ae07832e925f61aa8b479cec69a79
gparin <99040393+KIITTIKUN@users.noreply.github.com>
2023-07-13 00:50:52 +0700

    Time: 7243 ms (5.05%), Space: 42 MB (50.59%) - LeetHub

--- 0050-powx-n/0050-powx-n.js
+++ 0050-powx-n/0050-powx-n.js
@@ -4,8 +4,15 @@
  * @return {number}
  */
 const myPow = (x, n) => {
+    let result = 1;
     if(n===0){
         return 1
+    } 
+    else if (n > 1){
+        for(let i = 0;i<n;i++){
+            result *= x;
+        }
+        return result
     }
     return x**n
 };
