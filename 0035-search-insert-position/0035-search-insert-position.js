fb912c5728545b7de40179a6329d7a75fb4c5b6c
gparin <99040393+KIITTIKUN@users.noreply.github.com>
2023-07-12 15:50:05 +0700

    Time: 68 ms (11.43%), Space: 42.3 MB (41.41%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+const searchInsert = (nums, target) => {
+    let i = 0;
+    while(i < nums.length){     
+        if(target === nums[i]){
+            return i 
+        } else if (target < nums[i]){
+                   return i 
+        }
+        i++
+    }
+    return i
+};
