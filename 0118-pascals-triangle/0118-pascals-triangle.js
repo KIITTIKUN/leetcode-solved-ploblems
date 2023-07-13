55fa6b64e7fbc5ab782edaf03b209a8bcb8d66ea
gparin <99040393+KIITTIKUN@users.noreply.github.com>
2023-07-14 01:26:29 +0700

    Time: 53 ms (88.11%), Space: 42.6 MB (19.36%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+const generate = (numRows)=> {
+    
+    if(numRows === 1){
+        return [[1]]
+    }
+
+    const triangle = [] 
+    for(let i = 0;i<numRows;i++){
+        triangle.push([]);
+        for(let j =0; j <= i;j++){
+            if(j===0 || j === i){
+                triangle[i][j] = 1
+            } else {
+                triangle[i][j] = triangle[i-1][j-1] + triangle[i-1][j]
+            }
+        }
+    }
+    return triangle;
+    
+};
