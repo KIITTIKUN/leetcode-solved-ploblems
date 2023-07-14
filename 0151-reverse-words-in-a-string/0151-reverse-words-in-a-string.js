40fc05f5c86b4ae5e3fb11de1863e7c33b7c66fe
gparin <99040393+KIITTIKUN@users.noreply.github.com>
2023-07-14 09:23:30 +0700

    Time: 172 ms (5.04%), Space: 51 MB (5.18%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+const reverseWords = (string) => {
+     let splitStr = string.split(" ").filter(word=>word.length>0)
+    let reverseWord = "";
+    for(let i = splitStr.length -1 ;i>=0 ;i--){
+        if(i !== 0){
+            reverseWord += `${splitStr[i]} `
+        } else {
+            reverseWord += `${splitStr[i]}`
+        }
+        console.log(reverseWord)
+    }
+    return reverseWord
+};
