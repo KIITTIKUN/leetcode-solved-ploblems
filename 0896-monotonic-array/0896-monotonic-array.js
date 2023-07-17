d60e9dabfd8f80a87aa0d37a6093dcefd7727c0c
gparin <99040393+KIITTIKUN@users.noreply.github.com>
2023-07-18 02:20:05 +0700

    Time: 188 ms (9.42%), Space: 71.5 MB (5.14%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.js
+++ 0896-monotonic-array/0896-monotonic-array.js
@@ -1,18 +1,18 @@
 const isDecrease = (nums) => {
-    console.log(`check decrease array`)
+    // console.log(`check decrease array`)
     for(let i = 0 ; i < nums.length-1;i++){
         if(nums[i] < nums[i+1]){
-            console.log(`found increase because ${nums[i]} < ${nums[i+1]}`)
+            // console.log(`found increase because ${nums[i]} < ${nums[i+1]}`)
             return false
         }
     }
     return true
 }
 const isIncrease = (nums) => {
-    console.log(`check increase array`)
+    // console.log(`check increase array`)
     for(let i = 0 ; i < nums.length-1;i++){
         if(nums[i] > nums[i+1]){
-            console.log(`found decrease because ${nums[i]} > ${nums[i+1]}`)
+            // console.log(`found decrease because ${nums[i]} > ${nums[i+1]}`)
             return false
         }
     }
@@ -21,7 +21,7 @@ const isIncrease = (nums) => {
 const isMonotonic = (nums) => {
     console.log(`this sequence = {${nums}}`)
     let decrease = isDecrease(nums),increase = isIncrease(nums);
-    console.log(`isDecrease = ${decrease} and isIncrease = ${increase}`)
+    // console.log(`isDecrease = ${decrease} and isIncrease = ${increase}`)
     if( decrease === false && increase === false){
         return false
    }
