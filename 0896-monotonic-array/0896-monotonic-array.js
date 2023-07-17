073812226aa9e1fcfc824349e44c0be6d5dc4a93
gparin <99040393+KIITTIKUN@users.noreply.github.com>
2023-07-18 02:19:35 +0700

    Time: 206 ms (8.78%), Space: 71.7 MB (5.14%) - LeetHub

---
+++ 0896-monotonic-array/0896-monotonic-array.js
@@ -0,0 +1,30 @@
+const isDecrease = (nums) => {
+    console.log(`check decrease array`)
+    for(let i = 0 ; i < nums.length-1;i++){
+        if(nums[i] < nums[i+1]){
+            console.log(`found increase because ${nums[i]} < ${nums[i+1]}`)
+            return false
+        }
+    }
+    return true
+}
+const isIncrease = (nums) => {
+    console.log(`check increase array`)
+    for(let i = 0 ; i < nums.length-1;i++){
+        if(nums[i] > nums[i+1]){
+            console.log(`found decrease because ${nums[i]} > ${nums[i+1]}`)
+            return false
+        }
+    }
+    return true
+}
+const isMonotonic = (nums) => {
+    console.log(`this sequence = {${nums}}`)
+    let decrease = isDecrease(nums),increase = isIncrease(nums);
+    console.log(`isDecrease = ${decrease} and isIncrease = ${increase}`)
+    if( decrease === false && increase === false){
+        return false
+   }
+   
+   return true
+};
