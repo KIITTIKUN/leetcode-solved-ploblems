0f70f6b9547066f2d1203cc35dc74c472dbdb4db
gparin <99040393+KIITTIKUN@users.noreply.github.com>
2023-08-12 17:21:49 +0700

    Time: 60 ms (42.84%), Space: 42.6 MB (13.07%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.js
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.js
@@ -3,20 +3,24 @@
  * @return {number[]}
  */
 const getRow = (rowIndex) => {
-     if(rowIndex === 0){
-        return [[1]]
-    }
-
-    const triangle = [] 
-    for(let i = 0;i<=rowIndex;i++){
-        triangle.push([]);
-        for(let j =0; j <= i;j++){
-            if(j===0 || j === i){
-                triangle[i][j] = 1
-            } else {
-                triangle[i][j] = triangle[i-1][j-1] + triangle[i-1][j]
-            }
-        }
-    }
-    return triangle[rowIndex];
+  if(rowIndex === 0){
+      return [1]
+  }
+  
+  if(rowIndex === 1){
+      return [1,1]
+  }
+  
+  const result = [];
+  for(let i = 0;i<=rowIndex;i++){
+    result.push([])
+      for(let j = 0;j<=i;j++){
+          if(j === 0 || j === i){
+              result[i][j] = 1
+          }else{
+              result[i][j] = result[i-1][j-1] + result[i-1][j]
+          }
+      }
+  }
+  return result[result.length-1]
 };
